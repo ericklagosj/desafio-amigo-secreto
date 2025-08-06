bb077890bfdc10a60e4f311839a306efa95ed2e5
ericklagosj <erick.lagos1901@alumnos.ubiobio.cl>
2025-08-06 19:18:04 -0400

    funcionalidad de agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    const input =document.getElementById('amigo');
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+
+    //agregar el nombre al array amigos
+    amigos.push(nombre);
+
+    // Mostrar el nombre en la lista
+    const lista = document.getElementById('listaAmigos');
+    const li = document.createElement('li');
+    li.textContent = nombre;
+    lista.appendChild(li);
+
+    input.value = ""; // Limpiar el campo de entrada
+}
